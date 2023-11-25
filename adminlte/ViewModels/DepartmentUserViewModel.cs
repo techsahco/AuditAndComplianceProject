b877d75c2bfa6a564bb48d066d15546977f7dfc9
techsahco <146542926+techsahco@users.noreply.github.com>
2023-11-25 17:56:07 +0500

    department user creation issue resolved

--- adminlte/ViewModels/DepartmentUserViewModel.cs
+++ adminlte/ViewModels/DepartmentUserViewModel.cs
@@ -41,7 +41,7 @@ namespace adminlte.ViewModels
         public string ConfirmPassword { get; set; }
 
         public string userId { get; set; }
-        [Required]
+        //[Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
